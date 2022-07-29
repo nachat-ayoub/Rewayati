197b16b75bb045790cbe996b6d9f1cca7d09f937
Nachat Ayoub <venomx2020x@gmail.com>
2022-07-29 13:16:05 +0100

    New Commit

--- src/env.js
+++
@@ -1,3 +0,0 @@
-export const env = {
-	BASE_API_URL: process.env.BASE_API_URL
-};
--- vite.config.js
+++ vite.config.js
@@ -2,12 +2,7 @@ import { sveltekit } from '@sveltejs/kit/vite';
 
 /** @type {import('vite').UserConfig} */
 const config = {
-	plugins: [sveltekit()],
-	define: {
-		...(process.env.NODE_ENV === 'development' && {
-			'process.env': process.env
-		})
-	}
+	plugins: [sveltekit()]
 };
 
 export default config;
